333c152b77597877442ebcfe7578de04fd636d59
Emmanuel Umeh <emmachinemerem04@gmail.com>
2024-05-11 11:16:00 -0700

    first-deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common'
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,18 +10,4 @@ import { NgOptimizedImage } from '@angular/common'
 })
 export class AppComponent {
   title = 'Visual Feats Netlify Dynamic Site Hackethon';
-  imageUrl = 'https://unsplash.com/photos/photo-of-moon-pd4lo70LdbI';
 }
-
-// import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'Visual Feats Netlify Dynamic Site Hackethon';
-//   imageUrl = 'https://unsplash.com/photos/photo-of-moon-pd4lo70LdbI';
-// }
